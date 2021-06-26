6e96eca59ae273a18f50c3cf7277e11425f2d113
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-06-25 21:23:39 -0700

    debugging

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const Intern=require("./js/intern");
 const Manager=require("./js/manager");
 
 const employee=[];
+const engineerArr=[];
+const managerArr=[];
+const internArr=[];
 
 function startApp(){
-    generateHtml();
+    // generateHtml();
     addEmployees();
 }
 function addEmployees(){
@@ -59,23 +62,27 @@ function addEmployees(){
             name: "addMore"
         }])
         .then(function({positionDets, addMore}) {
+            console.log(name, id, email)
             let addMember;
             if (position === "Engineer") {
                 addMember = new Engineer(name, id, email, positionDets);
+                engineerArr.push(addMember);
             } else if (position === "Intern") {
                 addMember = new Intern(name, id, email, positionDets);
+                internArr.push(addMember);
             } else {
                 addMember = new Manager(name, id, email, positionDets);
+                managerArr.push(addMember);
             }
-            employee.push(addMember);
-            generateHtml(addMember)
-            .then(function() {
+            // employee.push(addMember)
+            // generateHtml(addMember)
+            // .then(function() {
                 if (addMore === "yes") {
-                    addMember();
+                    addEmployees();
                 } else {
                     completeHtml();
                 }
-            });
+            // });
             
         });
     });
@@ -104,14 +111,73 @@ function beginHtml() {
         }
     });
     console.log("begin");
+    beginHtml();
 }
 
-function generateHtml(member) {
+function createManager(){
+    let html="";
+for (let index = 0; index < managerArr.length; index++) {
+    const element = managerArr[index];
+    html+= `
+    <div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${element.name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${element.id}</li>
+                <li class="list-group-item">Email Address: ${element.email}</li>
+                <li class="list-group-item">Office Phone: ${element.officeNumber}</li>
+            </ul>
+            </div>
+        </div>
+    `
+}
+return html;
+}
+function createIntern(){
+    let html="";
+for (let index = 0; index < internArr.length; index++) {
+    const element = internArr[index];
+    html+= `
+    <div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${element.name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${element.id}</li>
+                <li class="list-group-item">Email Address: ${element.email}</li>
+                <li class="list-group-item">Office Phone: ${element.school}</li>
+            </ul>
+            </div>
+        </div>
+    `
+}
+return html;
+}
+function createEngineer(){
+    let html="";
+for (let index = 0; index < engineerArr.length; index++) {
+    const element = engineerArr[index];
+    html+= `
+    <div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${element.name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${element.id}</li>
+                <li class="list-group-item">Email Address: ${element.email}</li>
+                <li class="list-group-item">Office Phone: ${element.github}</li>
+            </ul>
+            </div>
+        </div>
+    `
+}
+return html;
+}
+
+function generateHtml(manager,engineer, intern) {
     return new Promise(function(resolve, reject) {
-        const name = member.getName();
+        // const name = member.getName();
         const position = member.getPosition();
-        const id = member.getId();
-        const email = member.getEmail();
+        // const id = member.getId();
+        // const email = member.getEmail();
         let info = "";
         if (position === "Engineer") {
             const gitHub = member.getGithub();
@@ -119,9 +185,9 @@ function generateHtml(member) {
             <div class="card mx-auto mb-3" style="width: 18rem">
             <h5 class="card-header">${name}<br /><br />Engineer</h5>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
+                <li class="list-group-item">ID: ${member.id}</li>
+                <li class="list-group-item">Email Address: ${member.email}</li>
+                <li class="list-group-item">GitHub: ${member.gitHub}</li>
             </ul>
             </div>
         </div>`;
@@ -167,18 +233,46 @@ function generateHtml(member) {
 }
 
 function completeHtml() {
-    const html = ` </div>
-    </div>
+    fs.writeFile("./results/member.html", `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1  text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">
+            ${createManager()}
+            ${createIntern()}
+            ${createEngineer()}
+            </div>
+            </div>
+            </body>
+            </html>
+    `, function (err) {
+                if (err) {
+                    console.log(err);
+                };
+            });
+//     const html = ` </div>
+//     </div>
     
-</body>
-</html>`;
+// </body>
+// </html>`;
 
-    fs.appendFile("./results/member.html", html, function (err) {
-        if (err) {
-            console.log(err);
-        };
-    });
-    console.log("end");
+//     fs.appendFile("./results/member.html", html, function (err) {
+//         if (err) {
+//             console.log(err);
+//         };
+//     });
+//     console.log("end");
 }
 
 startApp();
